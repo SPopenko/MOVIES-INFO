1dcee0b85a079b901a792af208f01febd57ed74a
Anton Poluboiarynov <apolwork@gmail.com>
2011-10-31 12:22:44 +0200

    [#17858531]  Caching
    * Added MovieCache Class
    * Added poster caching in ShortMovieInfoCell

---
+++ Movies-Info/Movies-Info/MovieCache.h
@@ -0,0 +1,13 @@
+//
+//  MovieCache.h
+//  Movies-Info
+//
+//  Created by Anton Poluboiarynov on 10/31/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MovieCache : NSObject
+
+@end
---
+++ Movies-Info/Movies-Info/MovieCache.m
@@ -0,0 +1,13 @@
+//
+//  MovieCache.m
+//  Movies-Info
+//
+//  Created by Anton Poluboiarynov on 10/31/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "MovieCache.h"
+
+@implementation MovieCache
+
+@end
