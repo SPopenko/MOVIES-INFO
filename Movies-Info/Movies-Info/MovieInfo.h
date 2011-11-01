bfb8e262e62f753e2c14f6dce097060e062c28f6
Anton Poluboiarynov <apolwork@gmail.com>
2011-11-01 12:23:14 +0200

    [#20407129] MappingRefactoring
    * Added class MovieInfo for getting info from themovidb.org

---
+++ Movies-Info/Movies-Info/MovieInfo.h
@@ -0,0 +1,13 @@
+//
+//  MovieInfo.h
+//  Movies-Info
+//
+//  Created by Anton Poluboiarynov on 11/1/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MovieInfo : NSObject
+
+@end
---
+++ Movies-Info/Movies-Info/MovieInfo.m
@@ -0,0 +1,13 @@
+//
+//  MovieInfo.m
+//  Movies-Info
+//
+//  Created by Anton Poluboiarynov on 11/1/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "MovieInfo.h"
+
+@implementation MovieInfo
+
+@end
