4f2e486842c1196e63c723211bedbce49d24480e
Anton Poluboiarynov <apolwork@gmail.com>
2011-11-02 14:06:36 +0200

    [Delivers #20407165] Added category for UIViewController
    * Moved equal code from MoviesInfoDetailed and MovieInfoTableView to ViewActionIndicator

---
+++ Movies-Info/Movies-Info/ViewActionIndicator.h
@@ -0,0 +1,13 @@
+//
+//  UIViewActioIndicator.h
+//  Movies-Info
+//
+//  Created by Anton Poluboiarynov on 11/2/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIViewActioIndicator : UIView
+
+@end
---
+++ Movies-Info/Movies-Info/ViewActionIndicator.m
@@ -0,0 +1,31 @@
+//
+//  UIViewActioIndicator.m
+//  Movies-Info
+//
+//  Created by Anton Poluboiarynov on 11/2/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "UIViewActioIndicator.h"
+
+@implementation UIViewActioIndicator
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
